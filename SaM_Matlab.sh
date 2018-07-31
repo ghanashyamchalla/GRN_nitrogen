46b7fa6f9f8b65f562c3c8708291c4d366d8473f
Stuti Shrivastava <stuti15@StutisMBP.local>
2018-07-31 13:27:17 -0500

    updated files

--- SaM_Matlab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# ----------------Your Commands------------------- #
-
-matlab -nodisplay -nosplash -r "addpath('./src'); SaM('./Input/SaM_input.txt','./Output/SaM_output.txt'); exit;"
--- src/DFG4GRN/GRN_ReadDatafile.m
+++ src/DFG4GRN/GRN_ReadDatafile.m
@@ -1,11 +1,8 @@
 function [dataKCL1, dataKCL2, dataKNO31, dataKNO32, dataKnown, deltaT, ...
-        nComb, KCl, KNO3, geneNames, TFNames, nonTFNames] = GRN_ReadDatafile(filedata)
+        nComb, KCl, KNO3, geneNames, TFNames, nonTFNames] = GRN_readDatafile(filedata)
     
     % Save all data as columns and rows
     all_rows = strsplit(filedata, '\r');
-    if (length(all_rows) == 1)
-        all_rows = strsplit(filedata, '\n');
-    end
     header_row = strsplit(all_rows{1}, '\t');
     nCols = length(header_row);
     nRows = length(all_rows);
@@ -16,24 +13,14 @@ function [dataKCL1, dataKCL2, dataKNO31, dataKNO32, dataKnown, deltaT, ...
         static_data(k,:) = regexp(all_rows{k}, '\t', 'split');
     end
      
-    geneNames = static_data(2:nRows,1);
-    TFNames = geneNames(1:(nRows-2),1)';
-    nonTFNames = geneNames((nRows-1),1)';
+    geneNames = static_data(2:15,1);
+    TFNames = geneNames(1:13,1)';
+    nonTFNames = geneNames(14,1)';
     
-%     KCl1_index = 2:2:(nCols+3)/2;
-%     KCl2_index = 3:2:(nCols+3)/2;
-%     KNO31_index = [2 (nCols+5)/2:2:nCols];
-%     KNO32_index = [3 (nCols+7)/2:2:nCols];
-    
-    KCl1_index = 2:8;
-    KCl2_index = 9:15;
-    KNO31_index = 16:22;
-    KNO32_index = 23:29;
-    
-    dataKCL1 = cellfun(@str2num, static_data(2:nRows,KCl1_index));
-    dataKCL2 = cellfun(@str2num, static_data(2:nRows,KCl2_index));
-    dataKNO31 = cellfun(@str2num, static_data(2:nRows, KNO31_index));
-    dataKNO32 = cellfun(@str2num, static_data(2:nRows, KNO32_index));
+    dataKCL1 = cellfun(@str2num, static_data(2:15,2:8));
+    dataKCL2 = cellfun(@str2num, static_data(2:15,9:15));
+    dataKNO31 = cellfun(@str2num, static_data(2:15, 16:22));
+    dataKNO32 = cellfun(@str2num, static_data(2:15, 23:29));
     
     dataKnown = {logical(dataKCL1),logical(dataKCL2),logical(dataKNO31),logical(dataKNO32)};
     
